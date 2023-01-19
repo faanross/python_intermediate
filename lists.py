be0dd91d329ead30b062e6366955541fec81a4c4
dithermedaddy <moi@faanross.com>
2023-01-19 08:57:49 -0500

    int python

--- lists.py
+++ lists.py
@@ -13,7 +13,28 @@ print(my_list2)
 print(my_list[2])
 print(my_list[-1])
 
-if "napoleon" in my_list:
-    print("It is in the list")
+test1 = "napoleon"
+test2 = "rex kwon do"
+
+if test1 in my_list:
+    print(test1 + " is in the list")
 else:
-    print("It is not in the list")
+    print(test1 + " is not in the list")
+
+if test2 in my_list:
+    print(test2 + " is in the list")
+else:
+    print(test2 + " is not in the list")
+
+amount = len(my_list)
+print("There are", amount,"elements in the list.")
+
+my_list.append("lawfawnduh")
+print(my_list)
+
+my_list.insert(0, "liger")
+print(my_list)
+
+removeme = my_list.pop()
+print(removeme)
+print(my_list)
