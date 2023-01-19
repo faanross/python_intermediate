f676a0e457408b47dd1c27689c2a6362e524b5b9
dithermedaddy <moi@faanross.com>
2023-01-19 08:47:37 -0500

    int python

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+# just messing around with lists as a refresher for Python intermediate course by Patrick Loeber
+
 my_list = ["napoleon", "kip", "pedro", 69, False, 12.420]
 print(my_list)
 
@@ -9,3 +11,9 @@ my_list2 = []
 print(my_list2)
 
 print(my_list[2])
+print(my_list[-1])
+
+if "napoleon" in my_list:
+    print("It is in the list")
+else:
+    print("It is not in the list")
