16ed9907dc482f9b289aee580645b157cb4763f3
dithermedaddy <moi@faanross.com>
2023-01-20 08:50:37 -0500

    tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are:
+# Ordered (same as lists)
+# Immutable (vs lists which are mutable)
+# Also allows duplicates (same as lists)
+# Thus major diff vs lists is that they are immutable
+# Use them in programs when you want to ensure variable value remains constant
+
+first_tuple = ("Kip", 32, "Cage Fighter")
+print(first_tuple)
