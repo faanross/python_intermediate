45f20e9d1f7e93d5d3ed6f8b67d86d3b93e09c08
dithermedaddy <moi@faanross.com>
2023-01-20 08:56:22 -0500

    tuples

--- tuples.py
+++ tuples.py
@@ -13,3 +13,17 @@ print(type(first_tuple))
 
 first_tuple = ("Kip",)
 print(type(first_tuple))
+
+# Remember you can nest: for ex lists in a tuple
+# Here we will be able to change the individual values in the lists, but cannot add or remove lists or change order
+
+first_tuple = (["Kip", 32, "Cage Fighter"], ["Rico", 44, "Pro Athlete"])
+print(first_tuple)
+
+# Accessing specific values
+
+print(first_tuple[0])
+print(first_tuple[1])
+
+print(first_tuple[0][2])
+print(first_tuple[1][0])
