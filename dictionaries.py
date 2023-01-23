b33a7f69b1a55ca0fba3147aa931cc90b500ab29
dithermedaddy <moi@faanross.com>
2023-01-23 18:12:40 -0500

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,8 @@
 # Dictionaries: Key:Value pairs, Unordered, Mutable, nesting provides many poss.
 
 first_dict = {"name": "Napoleon", "school": "Preston", "age": 17, "food": "quesadilla"}
+print(first_dict)
+
+#using dict function to caste dictionary
+second_dict = dict(name="Rico", school="Preston", age=46, food="minute steak")
+print(second_dict)
