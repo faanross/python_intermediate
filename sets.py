f0aae625b8e85ed91e436ebea2c7c7f62be7efa7
dithermedaddy <moi@faanross.com>
2023-01-24 12:51:46 -0500

    sets

--- sets.py
+++ sets.py
@@ -3,4 +3,15 @@
 
 myset = {1, 6, 9, 13, 26, 69, 420, 666}
 print(myset)
-print(myset[3])
+
+# if we have copies of an item in a list, only one will be interpreted as beign in set
+myset = {1, 6, 9, 13, 26, 69, 420, 666, 6, 9, 13, 26, 69}
+print(myset)
+
+# we can use function to turn a word into a set, each UNIQUE letter will be processed as a single entry in set, and reorders ARBITRARILY
+newset1 = set("rexkwondo")
+print(newset1)
+# output: {'d', 'o', 'n', 'w', 'k', 'r', 'e', 'x'}
+
+if 'x' in newset1: print("Present")
+else: print("Not present")
