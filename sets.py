1429382526384d5817cbc79419948ea5befd036e
dithermedaddy <moi@faanross.com>
2023-01-24 12:46:55 -0500

    sets

--- sets.py
+++ sets.py
@@ -1,4 +1,6 @@
 # a collection type that is: unordered, mutable, does NOT allow duplicates (disting. charac.)
-# same curly braces as dict, but does not contain key:values 
+# same curly braces as dict, but does not contain key:values just single entries
 
-myset = []vervr
+myset = {1, 6, 9, 13, 26, 69, 420, 666}
+print(myset)
+print(myset[3])
