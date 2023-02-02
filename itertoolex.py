47a4917cf50a72fe4325e5b667b5144b89e6bf8d
Faan Ross <moi@faanross.com>
2023-02-02 16:00:07 -0500

    itertools

--- itertoolex.py
+++ itertoolex.py
@@ -2,4 +2,7 @@
 # iterators are data types that can be used in a for loop, most common example is list but here are more specific use cases
 
 # product
-import itertools
+from itertools import product
+a = [13, 26]
+b = [6, 9]
+prod = product(a, b)
