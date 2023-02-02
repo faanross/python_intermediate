3c4e8d066e303216186be679d932c19d60f9e49f
Faan Ross <moi@faanross.com>
2023-02-02 15:57:52 -0500

    itertools

--- itertoolex.py
+++ itertoolex.py
@@ -1,1 +1,4 @@
 # itertools: product, permutations, combinations, accumulate, groupby, and infinite iterators
+# iterators are data types that can be used in a for loop, most common example is list but here are more specific use cases
+
+# product
