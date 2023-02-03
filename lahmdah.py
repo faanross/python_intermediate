28e8f97d9008a79156a7a28caf6c1888db826594
Faan Ross <moi@faanross.com>
2023-02-03 12:59:44 -0500

    lambda

--- lahmdah.py
+++ lahmdah.py
@@ -3,3 +3,5 @@
 # creates a function with some arguments, evaluates, and returns
 
 add10 = lambda x: x + 10 # we are creating a function, it receives x, it returns x + 10
+print(add10(5))
+print(add10(add10(5)))
