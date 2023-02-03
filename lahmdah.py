1d884f1ae388a8b6cd3f067c7457fbc37822cb9f
Faan Ross <moi@faanross.com>
2023-02-03 12:57:02 -0500

    lambda

--- lahmdah.py
+++ lahmdah.py
@@ -1,2 +1,5 @@
 # lambda arguments: expression
 # small, one-line, anonymous function 
+# creates a function with some arguments, evaluates, and returns
+
+add10 = lambda x: x + 10 # we are creating a function, it receives x, it returns x + 10
