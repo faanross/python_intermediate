f2346a4bb756f6c92d2499757f1b3a5095a96006
Faan Ross <moi@faanross.com>
2023-02-03 13:00:34 -0500

    lambda

--- lahmdah.py
+++ lahmdah.py
@@ -5,3 +5,9 @@
 add10 = lambda x: x + 10 # we are creating a function, it receives x, it returns x + 10
 print(add10(5))
 print(add10(add10(5)))
+
+# practically same as a normal function
+def addsten(x):
+    return x + 10
+
+print(addsten(5))
