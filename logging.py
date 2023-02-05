a6116a454c6e4fbb8c41ddc2f8a2f6ec75bec53e
Faan Ross <moi@faanross.com>
2023-02-05 11:20:51 -0500

    logging

--- logging.py
+++ logging.py
@@ -1,3 +1,9 @@
 # built-in features to add logging to our applications
 import logging
 
+# there are 5 different log levels we can log to
+# logging.debug('This is a debug message')
+# logging.debug('This is a debug message')
+# logging.debug('This is a debug message')
+# logging.debug('This is a debug message')
+# logging.debug('This is a debug message')
