4d8ea8d1425833e5e8a8d3c6ae1febabf4fc8f34
Faan Ross <moi@faanross.com>
2023-02-05 11:21:45 -0500

    logging

--- logging.py
+++ logging.py
@@ -3,7 +3,8 @@ import logging
 
 # there are 5 different log levels we can log to
 # logging.debug('This is a debug message')
-# logging.debug('This is a debug message')
-# logging.debug('This is a debug message')
-# logging.debug('This is a debug message')
-# logging.debug('This is a debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
+
