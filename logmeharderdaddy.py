6a7ecb5a4b2e4f0d3df44fb64532f64d60e2c126
Faan Ross <moi@faanross.com>
2023-02-06 17:40:01 -0500

    logging fin

--- logmeharderdaddy.py
+++ logmeharderdaddy.py
@@ -36,3 +36,11 @@ file_h.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(name)s - %(levelname)s  - %(message)s')
 stream_h.setFormatter(formatter)
 file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logger.warning('This is a warning')
+logger.error('this is an error')
+
+
