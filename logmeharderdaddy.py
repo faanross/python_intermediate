69dee70bda702c3c19d09fc84eb54328cf956715
Faan Ross <moi@faanross.com>
2023-02-06 17:26:05 -0500

    logging fin

--- logmeharderdaddy.py
+++ logmeharderdaddy.py
@@ -17,3 +17,4 @@ logging.critical('This is a critical message')
 
 # we can change basic settings of logging with basicConfig
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s  - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
