1acb1fcaaf39f0b3ee7a84e46d82706a9a08df37
Faan Ross <moi@faanross.com>
2023-02-07 16:05:12 -0500

    json aka jayzone

--- jayzone.py
+++ jayzone.py
@@ -1,3 +1,11 @@
 # We have a Python dictionary and want to convert it to JSON
 # This is also known as serialization, or encoding
 
+import json
+
+napoleon = {
+    "meal": "quesadilla",
+    "numbers": 169,
+    "sequence": [1, 2, 3],
+    "girslike": {"skills": "computer_hacking"}
+}
