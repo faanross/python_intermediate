c381ef2dbe9ef8aea2e23343d16ee56aff01cd2e
Faan Ross <moi@faanross.com>
2023-02-11 11:55:24 -0500

    decorators

---
+++ ledecoratorz.py
@@ -0,0 +1,7 @@
+# two different decorators: function decorators, and class decorators
+# function, shown immediately below, is more common
+# think of decorator as a function, that takes another 
+
+@mydecorator
+def dosummin():
+    pass
