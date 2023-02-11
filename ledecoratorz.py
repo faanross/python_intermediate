bf1604d67735edd742321ba8502bf25cd7a10047
Faan Ross <moi@faanross.com>
2023-02-11 12:00:12 -0500

    decorators

--- ledecoratorz.py
+++ ledecoratorz.py
@@ -1,7 +1,9 @@
 # two different decorators: function decorators, and class decorators
 # function, shown immediately below, is more common
-# think of decorator as a function, that takes another 
+# think of decorator as a function, that takes another function as an argument
+# it can thus extend the functionality of a function, without modifying it
 
-@mydecorator
-def dosummin():
-    pass
+# in principle the dosummin() function is extended by adding the functionality of @mydecorator
+# @mydecorator
+# def dosummin():
+#    pass
