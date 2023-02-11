761454fc08ddb3f4591046bc84fb3ccd6f1579ed
Faan Ross <moi@faanross.com>
2023-02-11 12:06:37 -0500

    decorators

--- ledecoratorz.py
+++ ledecoratorz.py
@@ -15,10 +15,11 @@ def start_end_decorator(func):
         print("Enjoy your day!")
     return wrapper
 
-@start_end_decorator
 def print_name(x):
     print("Hello, "+x)
 
 thename = input("What's your name? ")
+
+print_name = start_end_decorator(print_name)
 print_name(thename)
 
