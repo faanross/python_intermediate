893a24120323315eb0a871fc1085f503f686c2ae
Faan Ross <moi@faanross.com>
2023-02-12 15:52:44 -0500

    generator

--- legenerator.py
+++ legenerator.py
@@ -1,4 +1,7 @@
 # functions that return an object that can be iterated over
 # defined exactly like a normal function but instead of return keyword we use yield
 
-
+def mygenerator():
+    yield 1
+    yield 2
+    yield 3
