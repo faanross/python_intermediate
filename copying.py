c316dd129e09b4da2eeb74a981e2bcba3ae34f1a
Faan Ross <moi@faanross.com>
2023-02-15 17:33:36 -0500

    shallow vs deep copies

--- copying.py
+++ copying.py
@@ -6,3 +6,23 @@ org = 5
 cpy = org
 # this did not make a "real" copy, instead it simply made a new variable with the same refernce
 
+cpy = 6
+# this assignment again creates a new variable
+
+print(cpy)
+print(org)
+# we can see they are different
+
+# it is different with a list, we have to be more careful
+org_list = [0, 1, 2, 3, 5]
+cpy_list = org_list
+# so we copied it now if we want to make a change
+cpy_list[0] = 69
+
+print(cpy_list)
+print(org_list)
+# thus when we print we can see that BOTH were changed.
+# thus we want to be able to make a new, independent copy
+
+
+
