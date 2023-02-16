82974b26596a62347289423221c1058bc64ec944
Faan Ross <moi@faanross.com>
2023-02-16 16:14:16 -0500

    context manager

--- contextmanager.py
+++ contextmanager.py
@@ -3,7 +3,11 @@ with open('notes.txt', 'w') as file:
     user_text = input("What would you like to write? ")
     file.write(user_text)
 
+
 file = open('notes.txt', 'w')
 try:
     file.write('something else too...')
 finally: 
+    file.close()
+
+# finally will execute with or without an exception vs try that is dependent
